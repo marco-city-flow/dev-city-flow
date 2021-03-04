808fbedf0d6f54fc9437e0639b69336aa1909f05
qwerty250 <qiyue2001@gmail.com>
2021-03-04 16:29:37 +0800

    Update assign_engine.py
    Rewrite engine assignment algorithm, based on grouping of intersections

--- tools/generator/assign_engine.py
+++ tools/generator/assign_engine.py
@@ -20,7 +20,7 @@ def parse_args():
     return parser.parse_args()
 
 
-def group_by_length(roads, n):
+def group_roads_by_length(roads, n):
     lists = [[] for _ in range(n)]
     number_list = list(roads)
     totals = [(0, i) for i in range(n)]
@@ -33,7 +33,7 @@ def group_by_length(roads, n):
     return number_list, lists
 
 
-def group_by_midpoint(roads, n):
+def group_roads_by_midpoint(roads, n):
     kmeans = KMeans(n_clusters=n,
                     init='k-means++',
                     n_init=10,
@@ -51,6 +51,38 @@ def group_by_midpoint(roads, n):
     for _ in range(len(kmeans.labels_)):
         labels.append(int(kmeans.labels_[_]))
     return labels
+
+
+def polygonize(load_dict):
+    for intersection in load_dict['intersections']:
+        point = intersection['point']
+        # print((point['x']))
+        ix = point['x']
+        iy = point['y']
+        #print(ix,' ',iy)
+        dis = 0
+        for road in intersection['roads']:
+            for _ in load_dict['roads']:
+                if _['id'] == road:
+                    mx = _['midpoint']['x']
+                    my = _['midpoint']['y']
+                    length = math.sqrt((ix-mx)**2+((iy-my)**2))
+                    dis = dis+length
+        intersection['distanceToMidpoints'] = dis
+
+
+def group_intersections_by_length(intersections, n):
+    lists = [[] for _ in range(n)]
+    number_list = list(intersections)
+    totals = [(0, i) for i in range(n)]
+    heapq.heapify(totals)
+    for _ in range(len(intersections)):
+        total, index = heapq.heappop(totals)
+        lists[index].append(intersections[_])
+        heapq.heappush(
+            totals, (total + intersections[_]['distanceToMidpoints'], index))
+        number_list[_] = index
+    return number_list, lists
 
 
 if __name__ == '__main__':
@@ -78,17 +110,23 @@ if __name__ == '__main__':
             road['length'] = length
             road['midpoint'] = {'x': mx, 'y': my}
 
-    # Assign each road to 1 or 2 engine(s)
-    index_list_by_length, list_by_length = group_by_length(
+    # Polygonize each intersection
+    polygonize(load_dict)
+    index_list_polygon, list_polygon = group_intersections_by_length(
+        load_dict['intersections'], number_of_engines)
+
+    '''
+    # Assign each road to 1 or 2 engine(s), based on clustering of midpoint and length grouping
+    index_list_by_length, list_by_length = group_roads_by_length(
         load_dict['roads'], number_of_engines)
-    index_list_by_midpoint = group_by_midpoint(
+    index_list_by_midpoint = group_roads_by_midpoint(
         load_dict['roads'], number_of_engines)
 
     for _ in range(len(load_dict['roads'])):
         load_dict['roads'][_]['engine1'] = index_list_by_length[_]
         load_dict['roads'][_]['engine2'] = index_list_by_midpoint[_]
 
-    '''
+    
     for _ in range(number_of_engines):
         print(len(list_by_length[_]))
         print(sum(t['length'] for t in list_by_length[_]))
@@ -97,6 +135,20 @@ if __name__ == '__main__':
         print(pd.DataFrame(data=load_dict['roads'], columns=[
               'id', 'engine1', 'engine2', 'midpoint', 'length']))
     '''
+
+    # Assign each road to 1 or 2 engine(s), based on polygon grouping
+    for road in load_dict['roads']:
+        for _ in range(len(load_dict['intersections'])):
+            __ = load_dict['intersections'][_]
+            if __['id'] == road['startIntersection']:
+                engine1 = index_list_polygon[_]
+        for _ in range(len(load_dict['intersections'])):
+            __ = load_dict['intersections'][_]
+            if __['id'] == road['endIntersection']:
+                engine2 = index_list_polygon[_]
+        road['engine1'] = engine1
+        road['engine2'] = engine2
+        #print(road['id'], engine1, engine2)
 
     # Save JSON file
     if args.output:
