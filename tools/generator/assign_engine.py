3825759dbab7ce3418e10a8f50aa7a1b893487fd
qwerty250 <qiyue2001@gmail.com>
2021-03-05 20:54:55 +0800

    Update assign_engine.py
    now group intersections with clustering over their points, and is visualizable with matplotlib

--- tools/generator/assign_engine.py
+++ tools/generator/assign_engine.py
@@ -5,10 +5,12 @@ import math
 import heapq
 from sklearn.cluster import KMeans
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 # --roadnetFile Shuanglong.json --dir .\tools\generator
+# --roadnetFile roadnet_10_10.json --dir .\tools\generator
 
+z
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--rowNum", type=int)
@@ -53,13 +55,31 @@ def group_roads_by_midpoint(roads, n):
     return labels
 
 
+def group_intersections_by_point(intersections, n):
+    kmeans = KMeans(n_clusters=n,
+                    init='k-means++',
+                    n_init=10,
+                    max_iter=300,
+                    tol=0.0001,
+                    verbose=0,
+                    random_state=None,
+                    copy_x=True,
+                    algorithm='auto'
+                    )
+    points = pd.DataFrame(data=intersections, columns=['point'])[
+        'point'].apply(pd.Series)
+    kmeans.fit(points)
+    labels = list()
+    for _ in range(len(kmeans.labels_)):
+        labels.append(int(kmeans.labels_[_]))
+    return labels
+
+
 def polygonize(load_dict):
     for intersection in load_dict['intersections']:
         point = intersection['point']
-        # print((point['x']))
         ix = point['x']
         iy = point['y']
-        #print(ix,' ',iy)
         dis = 0
         for road in intersection['roads']:
             for _ in load_dict['roads']:
@@ -83,6 +103,27 @@ def group_intersections_by_length(intersections, n):
             totals, (total + intersections[_]['distanceToMidpoints'], index))
         number_list[_] = index
     return number_list, lists
+
+
+def scatter_intersections(intersections):
+    fig = plt.figure()
+    ax = plt.subplot()
+    colors = ['red', 'green', 'blue', 'yellow',
+              'cyan', 'gray', 'purple', 'orange']
+    for intersection in intersections:
+        x = intersection['point']['x']
+        y = intersection['point']['y']
+        engine = intersection['engine']
+        ax.scatter(x, y,  c=colors[engine], alpha=0.6)
+    plt.show()
+
+
+def distance_sum(intersections, n):
+    distances = [0 for i in range(n)]
+    for intersection in intersections:
+        distances[intersection['engine']
+                  ] = distances[intersection['engine']]+intersection['distanceToMidpoints']
+    return distances
 
 
 if __name__ == '__main__':
@@ -112,40 +153,47 @@ if __name__ == '__main__':
 
     # Polygonize each intersection
     polygonize(load_dict)
-    index_list_polygon, list_polygon = group_intersections_by_length(
+
+    #index_list_intersections, list_polygon = group_intersections_by_length(load_dict['intersections'], number_of_engines)
+
+    '''
+        # Assign each road to 1 or 2 engine(s), based on clustering of midpoint and length grouping
+        index_list_by_length, list_by_length = group_roads_by_length(
+            load_dict['roads'], number_of_engines)
+        index_list_by_midpoint = group_roads_by_midpoint(
+            load_dict['roads'], number_of_engines)
+
+        for _ in range(len(load_dict['roads'])):
+            load_dict['roads'][_]['engine1'] = index_list_by_length[_]
+            load_dict['roads'][_]['engine2'] = index_list_by_midpoint[_]
+
+        
+        for _ in range(number_of_engines):
+            print(len(list_by_length[_]))
+            print(sum(t['length'] for t in list_by_length[_]))
+
+        with pd.option_context('display.max_rows', 500, 'display.max_columns', 5):
+            print(pd.DataFrame(data=load_dict['roads'], columns=[
+                'id', 'engine1', 'engine2', 'midpoint', 'length']))
+    '''
+
+    index_list_intersections = group_intersections_by_point(
         load_dict['intersections'], number_of_engines)
 
-    '''
-    # Assign each road to 1 or 2 engine(s), based on clustering of midpoint and length grouping
-    index_list_by_length, list_by_length = group_roads_by_length(
-        load_dict['roads'], number_of_engines)
-    index_list_by_midpoint = group_roads_by_midpoint(
-        load_dict['roads'], number_of_engines)
+    # Save engine information of each engine
+    for _ in range(len(load_dict['intersections'])):
+        load_dict['intersections'][_]['engine'] = index_list_intersections[_]
 
-    for _ in range(len(load_dict['roads'])):
-        load_dict['roads'][_]['engine1'] = index_list_by_length[_]
-        load_dict['roads'][_]['engine2'] = index_list_by_midpoint[_]
-
-    
-    for _ in range(number_of_engines):
-        print(len(list_by_length[_]))
-        print(sum(t['length'] for t in list_by_length[_]))
-
-    with pd.option_context('display.max_rows', 500, 'display.max_columns', 5):
-        print(pd.DataFrame(data=load_dict['roads'], columns=[
-              'id', 'engine1', 'engine2', 'midpoint', 'length']))
-    '''
-
-    # Assign each road to 1 or 2 engine(s), based on polygon grouping
+    # Assign each road to 1 or 2 engine(s), based on grouping of connected intersections
     for road in load_dict['roads']:
         for _ in range(len(load_dict['intersections'])):
             __ = load_dict['intersections'][_]
             if __['id'] == road['startIntersection']:
-                engine1 = index_list_polygon[_]
+                engine1 = index_list_intersections[_]
         for _ in range(len(load_dict['intersections'])):
             __ = load_dict['intersections'][_]
             if __['id'] == road['endIntersection']:
-                engine2 = index_list_polygon[_]
+                engine2 = index_list_intersections[_]
         road['engine1'] = engine1
         road['engine2'] = engine2
         #print(road['id'], engine1, engine2)
@@ -156,3 +204,8 @@ if __name__ == '__main__':
     else:
         save_f = open(os.path.join(args.dir, 'output', args.roadnetFile), "w")
     json.dump(load_dict, save_f, indent=2)
+
+    print(distance_sum(load_dict['intersections'], number_of_engines))
+
+    # Scatter intersections
+    scatter_intersections(load_dict['intersections'])
